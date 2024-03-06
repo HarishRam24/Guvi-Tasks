2d396e4511812b74f9cf7575b885649352ed2f55
HarishRam24 <ram.harishv23@gmail.com>
2024-03-06 13:46:20 +0530

    Sample_Resume in JSON (for loops)

--- Hoisting/sample.js
+++ Hoisting/sample.js
@@ -3,12 +3,32 @@ const resumeobj = {
     education : "BE.MechanicalEngineering",
     about : "I'm a MechanicalEngineering graduate looking for an oppurtunity to showcase my skills.",
     skills : ["Autocad", "SolidWorks", "Ansys", "Catia", "NXcam"]
-} 
-
+};
+ 
 // for loop 
 const objKeys = Object.keys(resumeobj);
 const objValues = Object.values(resumeobj);
-for (let i = 0; i < resumeobj.length; i++) {
-    console.log("objKeys", objKeys[i]);
-    console.log("objValues", resumeobj[`${objKeys[i]}`]);
+for (let i = 0; i < objValues.length; i++) {
+    console.log(objKeys[i]);
+    console.log(objValues[i]);
   }
+
+// for each 
+objValues.forEach((val,idx) => {
+    console.log(val);
+    console.log(idx);
+}
+); 
+
+// for in 
+for (key in resumeobj) {
+    console.log(`
+    obj key : ${key}
+    obj value : ${resumeobj[key]}
+    `);
+}
+
+// for of 
+for (let key in resumeobj) {
+    console.log(`${key}: ${resumeobj[key]}`);
+}
