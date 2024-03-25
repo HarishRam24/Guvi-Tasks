a07b42bb48e6355de7eafc28b0c77b7643b192d5
HarishRam24 <ram.harishv23@gmail.com>
2024-03-25 19:21:26 +0530

    Tasks_Callback_Hell

--- Tasks_CallbackHell/script.js
+++ Tasks_CallbackHell/script.js
@@ -1,34 +1,39 @@
-// Countdown from 10 to 1
-setTimeout(() => {
-    console.log(10);
-    setTimeout(() => {
-        console.log(9);
-        setTimeout(() => {
-            console.log(8);
-            setTimeout(() => {
-                console.log(7);
-                setTimeout(() => {
-                    console.log(6);
-                    setTimeout(() => {
-                        console.log(5);
-                        setTimeout(() => {
-                            console.log(4);
-                            setTimeout(() => {
-                                console.log(3);
-                                setTimeout(() => {
-                                    console.log(2);
-                                    setTimeout(() => {
-                                        console.log(1);
-                                        setTimeout(() => {
-                                            console.log("Happy Independence Day");
-                                        }, 1000);
-                                    }, 1000);
-                                }, 1000);
-                            }, 1000);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}, 1000);
+var div=document.getElementById('timer')
+ 
+setTimeout(()=>{
+     div.innerHTML=10
+       setTimeout(()=>{
+           div.innerHTML=9
+           setTimeout(()=>{
+              div.innerHTML=8
+               setTimeout(()=>{
+                   div.innerHTML=7
+                   setTimeout(()=>{
+                      div.innerHTML=6
+                       setTimeout(()=>{
+                          div.innerHTML=5
+                           setTimeout(()=>{
+                             div.innerHTML=4
+                               setTimeout(()=>{
+                                  div.innerHTML=3
+                                   setTimeout(()=>{
+                                      div.innerHTML=2
+                                       setTimeout(()=>{
+                                          div.innerHTML=1
+                                           setTimeout(()=>{
+                                               div.innerHTML=0
+                                               setTimeout(()=>{
+                                                   div.innerHTML='Happy Independence Day';
+                                               },1000)
+                                           },1000)
+                                       },1000)
+                                   },1000)
+                                 
+                               },1000)
+                           },1000)
+                       },1000)
+                   },1000)
+               },1000)
+           },1000)
+       },1000)
+   },1000)
